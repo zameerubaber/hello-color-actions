db499d30f1ea0febc9f4cc63ff87108788fce16e
baberhood <zameerubaber@protonmail.com>
2023-11-09 17:00:11 -0600

    Update Message in Hello Test

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Zameer Baber who lives in Chicago");
+    expect(hello.hello()).toEqual("Hello World from Zameer Baber who lives in Chicago!");
   });
 });
